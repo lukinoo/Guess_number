95e96c30f29a4e1e78b20f25601bc2a5f9e5a942
luka bliadzee <74107662+lukinoo@users.noreply.github.com>
2022-03-20 17:18:28 +0400

    update 🔧

--- main.rb
+++ main.rb
@@ -7,12 +7,12 @@ while game_state
     # computer random numbers
     computer_n = rand(50)
     # player number
-    print "Type in your any Number between (0 / 50): "
+    print "Type any Number between (0 / 50): "
     player_n = gets.to_i
     # = Game options =
     if player_n == computer_n
         game_state = false
-        puts "You wind! player number #{player_n} compuer number #{computer_n}"
+        puts "You win! player number #{player_n} compuer number #{computer_n}"
     elsif player_n < computer_n
         puts "Player number #{player_n} is less then #{computer_n} computer number"
     else
